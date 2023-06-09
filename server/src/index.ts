3cf10f9619d7a4c0ff8b35d302d39e80a43802e2
lingsat <lingsatpilingast@gmail.com>
2023-06-09 07:59:57 +0300

    feat: return front from node

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,20 +3,32 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import taskRouter from './routers/taskRouter.js';
+import path from 'path';
 import 'dotenv/config';
 
+// Create Server
 const app = express();
 
+// Connect to MongoDb
 mongoose.set('strictQuery', false);
 mongoose.connect(`${process.env.MONGO_DB_URL}`);
 
+// Cors / Body Parser
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (_, res) => res.json({ message: 'Hello World' }));
+// Return Web page
+const __dirname = path.resolve();
+app.use(express.static('public'));
+app.get('/', (_, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
+// Api Routes
 app.use('/api/task', taskRouter);
 
+// Start Server
 const port = process.env.PORT || 4200;
 const start = async () => {
   try {
@@ -26,5 +38,4 @@ const start = async () => {
     console.error(`Error on server startup: ${(error as Error).message}`);
   }
 };
-
 start();
